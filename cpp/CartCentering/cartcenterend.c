b75497151553ea00ed3f903368c7f7a3d01c4581
carvalhosamuel <samfdm@gmail.com>
2022-01-10 13:51:43 +0000

    Add files via upload

---
+++ cpp/CartCentering/cartcenterend.c
@@ -0,0 +1,16 @@
+
+    > 0.0)
+    {
+      velocityChange = posForceVelChange;
+    }
+    else {
+      velocityChange = negForceVelChange;
+    }
+
+    stepSim();
+    }
+
+    printf("%f %d\n", sumFitCaseTimes, hitCount);
+
+    return (0);
+    }
